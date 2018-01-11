d5e68799ba9ac0b40160bb7b11aed3fea07ab84b
Brian Fletcher <brian.fletcher@arkanosoft.com>
2018-01-11 16:18:50 -0300

    I hope this works

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {Hero} from '../Hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  constructor(private heroService: HeroService) { }
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+  // heroes = HEROES;
+  heroes: Hero[];
+
+  selectedHero: Hero;
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+
+onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
+
+ngOnInit() {
+this.getHeroes();
+}
+
+}
